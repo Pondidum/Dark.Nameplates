f6d04acfda99e48912d8a3d2a8fc16e7598841b0
andy <andy@stormbase.net>
2013-05-18 17:15:38 +0100

    finder: changed to use select() rather than creating a table each time

--- finder.lua
+++ finder.lua
@@ -18,8 +18,12 @@ local plateFinder = {
 
 		local onUpdate = function()
 
-			for i, frame in ipairs({WorldFrame:GetChildren()}) do
-			
+			local count = select("#", WorldFrame:GetChildren())
+
+			for i = 1, count do
+				
+				local frame = select(i, WorldFrame:GetChildren())
+
 				if isPlate(frame) and not found[frame] then
 
 					found[frame] = true
