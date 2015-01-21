5c6e6fd892137da9e46b7e2cbaea172adc2497d6
andy <andy@andydote.co.uk>
2015-01-21 22:53:03 +0000

    colorPicker: inline function

--- colorPicker.lua
+++ colorPicker.lua
@@ -1,34 +1,27 @@
 local addon, ns = ...
 
-local colors = Dark.core.colors
+local colors = ns.lib.media.colors
 local reactionColors = colors.reaction
 
- local fixColor = function(r, g, b)
-    r = floor(r * 100 + .5) / 100
-    g = floor(g * 100 + .5) / 100
-    b = floor(b * 100 + .5) / 100
-
-    return r, g, b
-end
-
 local colorPicker = {
-	
-	fix = fixColor,
 
 	fromUnitHealth = function(r, g, b)
 
-		r, g, b = fixColor(r, g, b)
+		-- fix colours
+		r = floor(r * 100 + .5) / 100
+	    g = floor(g * 100 + .5) / 100
+	    b = floor(b * 100 + .5) / 100
 
 		if g > .9 and r == 0 and b == 0 then 		-- friendly NPC
-			
+
 			r, g, b = unpack(reactionColors[5])
 
 		elseif b > .9 and r == 0 and g == 0 then 	-- friendly player
-			
+
 			r, g, b = 0, .3, .6
 
 		elseif r > .9 and g == 0 and b == 0 then 	-- enemy NPC
-								
+
 			r, g, b = unpack(reactionColors[1])
 
 		elseif (r + g) > 1.8 and b == 0 then 		-- neutral NPC
@@ -40,7 +33,7 @@ local colorPicker = {
 			r, g, b = .5, .5, .5
 
 		end											-- enemy player, use default UI colour
-		
+
 		return r, g, b
 
 	end,
@@ -48,11 +41,11 @@ local colorPicker = {
 	fromUnitThreat = function(r, g, b)
 
 		if g + b == 0 then 			-- aggro
-			
+
 			return 0.95, 0.2, 0.2
 
 		elseif b == 0 then			-- high threat
-			
+
 			return 0.95, 0.95, 0.2
 
 		else 						-- low/none
